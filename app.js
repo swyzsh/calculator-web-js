dccd90021b2497b89dcda5c329cd2062d32ab171
Swyzsh <swyzsh@protonmail.com>
2024-04-14 20:29:17 +0530

    add calculator logic

--- app.js
+++ app.js
@@ -1,5 +1,133 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
+
+  const calculate = (n1, operator, n2) => {
+    const firstNum = parseFloat(n1)
+    const secondNum = parseFloat(n2)
+    if (operator === 'add') return firstNum + secondNum
+    if (operator === 'subtract') return firstNum - secondNum
+    if (operator === 'multiply') return firstNum * secondNum
+    if (operator === 'divide') return firstNum / secondNum
+  }
   
+  const getKeyType = key => {
+    const { action } = key.dataset
+    if (!action) return 'number'
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) return 'operator'
+    // For everything else, return the action
+    return action
+  }
+  
+  const createResultString = (key, displayedNum, state) => {
+    const keyContent = key.textContent
+    const keyType = getKeyType(key)
+    const {
+      firstValue,
+      operator,
+      modValue,
+      previousKeyType
+    } = state
+  
+    if (keyType === 'number') {
+      return displayedNum === '0' ||
+        previousKeyType === 'operator' ||
+        previousKeyType === 'calculate'
+        ? keyContent
+        : displayedNum + keyContent
+    }
+  
+    if (keyType === 'decimal') {
+      if (!displayedNum.includes('.')) return displayedNum + '.'
+      if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'
+      return displayedNum
+    }
+  
+    if (keyType === 'operator') {
+      return firstValue &&
+        operator &&
+        previousKeyType !== 'operator' &&
+        previousKeyType !== 'calculate'
+        ? calculate(firstValue, operator, displayedNum)
+        : displayedNum
+    }
+  
+    if (keyType === 'clear') return 0
+  
+    if (keyType === 'calculate') {
+      return firstValue
+        ? previousKeyType === 'calculate'
+          ? calculate(displayedNum, operator, modValue)
+          : calculate(firstValue, operator, displayedNum)
+        : displayedNum
+    }
+  }
+  
+  const updateCalculatorState = (key, calculator, calculatedValue, displayedNum) => {
+    const keyType = getKeyType(key)
+    const {
+      firstValue,
+      operator,
+      modValue,
+      previousKeyType
+    } = calculator.dataset
+  
+    calculator.dataset.previousKeyType = keyType
+  
+    if (keyType === 'operator') {
+      calculator.dataset.operator = key.dataset.action
+      calculator.dataset.firstValue = firstValue &&
+        operator &&
+        previousKeyType !== 'operator' &&
+        previousKeyType !== 'calculate'
+        ? calculatedValue
+        : displayedNum
+    }
+  
+    if (keyType === 'calculate') {
+      calculator.dataset.modValue = firstValue && previousKeyType === 'calculate'
+        ? modValue
+        : displayedNum
+    }
+  
+    if (keyType === 'clear' && key.textContent === 'AC') {
+      calculator.dataset.firstValue = ''
+      calculator.dataset.modValue = ''
+      calculator.dataset.operator = ''
+      calculator.dataset.previousKeyType = ''
+    }
+  }
+  
+  const updateVisualState = (key, calculator) => {
+    const keyType = getKeyType(key)
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+  
+    if (keyType === 'operator') key.classList.add('is-depressed')
+    if (keyType === 'clear' && key.textContent !== 'AC') key.textContent = 'AC'
+    if (keyType !== 'clear') {
+      const clearButton = calculator.querySelector('[data-action=clear]')
+      clearButton.textContent = 'CE'
+    }
+  }
+  
+  const calculator = document.querySelector('.calculator')
+  const display = calculator.querySelector('.calculator__display')
+  const keys = calculator.querySelector('.calculator__keys')
+  
+  keys.addEventListener('click', e => {
+    if (!e.target.matches('div')) return
+    const key = e.target
+    const displayedNum = display.textContent
+    const resultString = createResultString(key, displayedNum, calculator.dataset)
+  
+    display.textContent = resultString
+    updateCalculatorState(key, calculator, resultString, displayedNum)
+    updateVisualState(key, calculator)
+  })
+
   /* Switch between various stylesheets */
   document.getElementById('switch-style-button').addEventListener('click', function() {
     let themeLink = document.getElementById('current-theme');
@@ -19,8 +147,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   /* Copy results button logic */
   const copyButton = document.getElementById('copy-result-btn');
-  let textToCopy = document.getElementById('result').textContent;
   copyButton.addEventListener("click", async () => {
+    let textToCopy = display.textContent;
     try {
       // Try the modern Clipboard API first (if supported)
       await navigator.clipboard.writeText(textToCopy);
@@ -41,4 +169,19 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+  /* Hyperlinks */
+  const githubBtn = document.getElementById('github-btn');
+  const twitterBtn = document.getElementById('twitter-btn');
+  const mailBtn = document.getElementById('mail-btn');
+  githubBtn.addEventListener('click', function() {
+    window.open('https://github.com/swyzsh', '_self');
+  });
+  twitterBtn.addEventListener('click', function() {
+    window.open('https://twitter.com/swyzsh', '_self');
+  });
+  mailBtn.addEventListener('click', function() {
+   let mail = document.createElement("a");
+   mail.href = "mailto:swyzsh@protonmail.com";
+   mail.click();
+  });
 });
